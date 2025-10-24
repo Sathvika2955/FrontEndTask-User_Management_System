d67e36dc9701b5f4cdef211f3415c419287717de
Sathvika Nidadavolu <sathvika2955@gmail.com>
2025-10-24 11:52:46 +0530

    implemented Figma-style logo, 2025 and ESTD under logo box

--- campus-recruitment-vite/src/components/AppHeader.jsx
+++ campus-recruitment-vite/src/components/AppHeader.jsx
@@ -1,34 +1,35 @@
 import React from 'react';
-import { Container, Nav } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
 import { FiSettings, FiUser } from 'react-icons/fi';
 
 const AppHeader = () => {
   return (
-    <header className="custom-header p-0 m-0">
-      <Container
-        fluid
-        className="d-flex justify-content-between align-items-center pt-4"
-        style={{ maxWidth: "900px" }}
-      >
-        <div className="text-center mx-auto w-100">
-          {/* LOGO in box */}
-          <div className="logo-box mx-auto mb-1">
-            <span className="logo-text">LOGO</span>
-          </div>
-          {/* ESTD 2025 */}
-          <div className="estd-text">ESTD 2025</div>
+    <header
+      className="custom-header"
+      style={{
+        width: "100%",
+        minHeight: 100,
+        display: "flex",
+        alignItems: "flex-start",
+        justifyContent: "space-between",
+        padding: "16px 32px"
+      }}
+    >
+      <div className="logo-stack">
+        <div className="logo-box">
+          <span className="logo-text">LOGO</span>
         </div>
-        {/* Right-side icons */}
-        <Nav className="position-absolute top-0 end-0 p-4">
-          <Nav.Link href="#settings" className="mx-2 text-dark">
-            <FiSettings size={22} />
-          </Nav.Link>
-          <Nav.Link href="#profile-icon" className="text-dark">
-            <FiUser size={22} />
-          </Nav.Link>
-        </Nav>
-      </Container>
-      <hr className="m-0" />
+        <div className="logo-year mt-1">2025</div>
+        <div className="logo-estd">ESTD</div>
+      </div>
+      <Nav className="pt-1">
+        <Nav.Link href="#settings" className="mx-2 text-dark">
+          <FiSettings size={22} />
+        </Nav.Link>
+        <Nav.Link href="#profile-icon" className="text-dark">
+          <FiUser size={22} />
+        </Nav.Link>
+      </Nav>
     </header>
   );
 };
